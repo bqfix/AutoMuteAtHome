3ca0fafde52fdfea4d14f42901cd4ca67f22fef6
bqfix <benjaminfix@gmail.com>
2019-01-23 16:15:47 -0500

    Added logic for database entry class
    Place

--- app/src/main/java/com/example/android/automuteathome/database/Place.java
+++ app/src/main/java/com/example/android/automuteathome/database/Place.java
@@ -1,6 +1,30 @@
 package com.example.android.automuteathome.database;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
 public class Place {
 
+@PrimaryKey(autoGenerate = true)
+private int id;
+private boolean muteOnEnter;
+private boolean muteOnExit;
 
+    public Place(boolean muteOnEnter, boolean muteOnExit) {
+        this.muteOnEnter = muteOnEnter;
+        this.muteOnExit = muteOnExit;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isMuteOnEnter() {
+        return muteOnEnter;
+    }
+
+    public boolean isMuteOnExit() {
+        return muteOnExit;
+    }
 }
